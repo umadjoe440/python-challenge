84e295859d93fc21c12797d2c7fd098165542e97
Joe Patton <umadjoe@gmail.com>
2019-11-11 17:33:30 -0500

    started challenge 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,14 @@
 # main.py - PyBank
+
+import csv
+import os
+
+csvpath = os.path.join("c:/Resources/00-Homework_03-Python_Instructions_PyBank_Resources_budget_data.csv")
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #Read and print the header row...
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
