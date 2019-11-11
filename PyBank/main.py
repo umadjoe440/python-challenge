6257c354159ae19fd318c7233180ead054588939
Joe Patton <umadjoe@gmail.com>
2019-11-11 18:14:33 -0500

    good progress on PyBank challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,3 +12,19 @@ with open(csvpath, newline='') as csvfile:
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+
+    row_list = []
+    months = 0
+    total_money = 0
+    for row in csvreader:
+        months += 1
+        total_money += float(row[1])
+        row_list.append((row[0],row[1]))
+        # The total number of months included in the dataset
+        # The net total amount of "Profit/Losses" over the entire period
+        # The average of the changes in "Profit/Losses" over the entire period
+        # The greatest increase in profits (date and amount) over the entire period
+        # The greatest decrease in losses (date and amount) over the entire period
+
+    print(f"The total number of months is:  {months}")
+    print(f"The total money is:  {total_money}")
