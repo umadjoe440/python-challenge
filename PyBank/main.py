ad696adb4c005586a54f494164c2c94d39ad2588
Joe Patton <umadjoe@gmail.com>
2019-11-13 18:25:10 -0500

    fixed diff loop

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,6 +14,7 @@ with open(csvpath, newline='') as csvfile:
     print(f"CSV Header: {csv_header}")
 
     row_list = []
+    monthly_list = []
     months = 0
     total_money = 0
     for row in csvreader:
@@ -21,15 +22,19 @@ with open(csvpath, newline='') as csvfile:
         total_money += float(row[1])
         row_list.append((row[0],row[1]))
         
-        
         # The average of the changes in "Profit/Losses" over the entire period
         # The greatest increase in profits (date and amount) over the entire period
         # The greatest decrease in losses (date and amount) over the entire period
-    #for r in row_list:
-    #    start_profit = r[1]
-    #    if row_list.index(r) > 0:
-    #        print(row_list.index(r))
-    #        end_profit = r[1]
+    for r in row_list:
+        if row_list.index(r) == 0:
+            start_profit = float(r[1])
+        else:
+            end_profit = float(r[1])
+            print(f"end_profit {end_profit} start profit {start_profit}")
+            monthly_diff = end_profit - start_profit
+            monthly_list.append((r[0],monthly_diff))
+            start_profit = end_profit
+            
     # using min() and max() 
 # to get min and max in list of tuples 
     #JGP generate another list of tuples with month and monthly profit/loss
@@ -41,3 +46,4 @@ with open(csvpath, newline='') as csvfile:
     print(f"The total number of months is:  {months}")
     # The net total amount of "Profit/Losses" over the entire period
     print(f"The total money is:  {total_money}")
+    #print(monthly_list)
