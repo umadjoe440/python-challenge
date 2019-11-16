0a5ee1c9ca485dcd11bc4ffc3279aab9f7552947
Joe Patton <umadjoe@gmail.com>
2019-11-16 08:19:17 -0500

    first assignment correct

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@
 
 import csv
 import os
+import operator
 
 csvpath = os.path.join("c:/Resources/budget_data.csv")
 
@@ -30,7 +31,7 @@ with open(csvpath, newline='') as csvfile:
             start_profit = float(r[1])
         else:
             end_profit = float(r[1])
-            print(f"end_profit {end_profit} start profit {start_profit}")
+            #print(f"end_profit {end_profit} start profit {start_profit}")
             monthly_diff = end_profit - start_profit
             monthly_list.append((r[0],monthly_diff))
             start_profit = end_profit
@@ -42,8 +43,29 @@ with open(csvpath, newline='') as csvfile:
     #res1 = min(test_list)[0], max(test_list)[0] 
     #res2 = min(test_list)[1], max(test_list)[1] 
 
+    print("Financial Analysis")
+    print("----------------------------")
+    print()
+
     # The total number of months included in the dataset
-    print(f"The total number of months is:  {months}")
+    print(f"Total Months:  {months}")
     # The net total amount of "Profit/Losses" over the entire period
-    print(f"The total money is:  {total_money}")
+    print(f"Total: $ {total_money}")
     #print(monthly_list)
+
+    #calculate average
+    sum_monthly_changes = sum([m[1] for m in monthly_list])
+    average_change = sum_monthly_changes/len(monthly_list)
+    print(f"Average Change:  {average_change}")
+
+    #greatest increase and decrease
+    greatest_increase_month = max(monthly_list,key=operator.itemgetter(1))[0]
+    greatest_increase_amount = max(monthly_list,key=operator.itemgetter(1))[1]
+    greatest_decrease_month = min(monthly_list,key=operator.itemgetter(1))[0]
+    greatest_decrease_amount = min(monthly_list,key=operator.itemgetter(1))[1]
+
+    print(f"Greatest Increase in Profits: {greatest_increase_month} {greatest_increase_amount}")
+    print(f"Greatest Decrease in Profits: {greatest_decrease_month} {greatest_decrease_amount}")
+
+    for month in monthly_list:
+        print(month)
